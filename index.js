12c922a1e7431e15be3c1da9f3a71ab504d47bc7
mdalimujtaba <mdalimujtaba29850@gmil.com>
2023-01-16 16:54:31 +0530

    edited

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express=require("express")
 const { connection } = require("./configs/db")
+const cors=require('cors')
 const { authenticate } = require("./middleware/authentication.middlware")
 const { postRoute } = require("./route/post.route")
 const { userRoute } = require("./route/user.route")
 require("dotenv").config()
 const app=express()
 app.use(express.json())
-
+app.use(cors())
 app.get("/",(req,res)=>{
     res.send("Home Page")
 })
